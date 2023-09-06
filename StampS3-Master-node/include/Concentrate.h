73744c1fdfeab83899b63424c283d402f37ed8a7
wicked-rainman <40294416+wicked-rainman@users.noreply.github.com>
2023-09-06 13:32:58 +0100

    Create Concentrate.h

---
+++ StampS3-Master-node/include/Concentrate.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <string.h>
+#include "defines.h"
+bool Concentrate(uint8_t *);
+extern uint8_t DedupeBufShort[][12];
+extern uint8_t DedupeBufLong[][18];
+extern bool RollingMac(uint8_t);
